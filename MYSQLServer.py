50e3442758c8820173c4a72ab3df79f38463b974
Betsy Rotich <betsyrotich022@gmail.com>
2024-07-26 14:58:40 +0300

    Building Database

--- MYSQLServer.py
+++ MYSQLServer.py
@@ -1,26 +1,30 @@
 import mysql.connector
 
-def create_database():
-    try:
         # Connect to MySQL server
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Betsyme234!",
-            port=3307
-        )
-        cursor = mydb.cursor()
+mydb = mysql.connector.connect(
+      
+     host="localhost",
+     user="root",
+     password="Betsyme234!",
+     port=3307
+ )
+cursor = mydb.cursor()
+
+def create_database():
+        try:
+              query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+              cursor.execute(query)
+              mydb.commit()
+              print("Database 'alx_book_store' created successfully!")
+
         
-        # Create database
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
+        except mysql.connector.Error as err:
+         
+         print(f"Error: {err}")
         
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        
-    finally:
+        finally:
         # Close the connection
-        if 'mydb' in locals() and mydb.is_connected():
+          if 'mydb' in locals() and mydb.is_connected():
             cursor.close()
             mydb.close()
 
