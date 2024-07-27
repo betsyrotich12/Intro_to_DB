2895ebe8a97314d31aa362dac6b324bda889e912
Betsy Rotich <betsyrotich022@gmail.com>
2024-07-27 14:57:49 +0300

    Script that describes table books

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,16 @@
 USE alx_book-store;
 
-SELECT * FROM Books;
+-- Use the specified database
+USE alx_book_store;
+
+-- Query the information schema to get the table structure
+SELECT
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Information'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
